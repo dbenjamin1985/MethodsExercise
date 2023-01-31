cd54693c441071849916b998ed14fa9fb1aa563e
David Benjamin <68559064+dbenjamin1985@users.noreply.github.com>
2023-01-31 15:00:59 -0700

    Update Methods.cs

--- MethodsExercise/Methods.cs
+++ MethodsExercise/Methods.cs
@@ -13,15 +13,15 @@ namespace MethodsExercise
 			int sum = num1 - num2;
 			return sum;
 		}
-        public static int Multiply(int num1, int num2)
-        {
-            int sum = num1 * num2;
-            return sum;
-        }
-        public static int Divide(int num1, int num2)
-        {
-            int sum = num1 / num2;
-            return sum;
-        }
-    }
+        	public static int Multiply(int num1, int num2)
+        	{
+           		int sum = num1 * num2;
+            		return sum;
+        	}
+        	public static int Divide(int num1, int num2)
+        	{
+            		int sum = num1 / num2;
+            		return sum;
+        	}
+    	}
 }
