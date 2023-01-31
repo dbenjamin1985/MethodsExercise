91bd8219f446925614004477bd0829681412626b
David Benjamin <davidbenjamin0685@gmail.com>
2023-01-31 14:56:19 -0700

    Added exercise 2

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MethodsExercise
+{
+	public class Methods
+	{
+		public static int Add(int num1, int num2)
+		{
+			int sum = num1 + num2;
+			return sum;
+		}
+		public static int Sub(int num1, int num2)
+		{
+			int sum = num1 - num2;
+			return sum;
+		}
+        public static int Multiply(int num1, int num2)
+        {
+            int sum = num1 * num2;
+            return sum;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            int sum = num1 / num2;
+            return sum;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
             
         {
+            Console.WriteLine("Exercise 1");
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("What is your favorite hockey team?");
@@ -14,8 +15,22 @@
             Console.WriteLine("Where is your favorite place to visit?");
             string place = Console.ReadLine();
             Console.WriteLine();
+
             Console.WriteLine($"Cool name {name}, I see that the {team} is your favorite hockey team but I don't like hockey." +
-                $"\n I'll go to {place} while you hang out with your buddy {friend} in hockey land. Later alligator!");
+                $"\nI'll go to {place} while you hang out with your buddy {friend} in hockey land. Later alligator!");
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise 2");
+            int answer1 = Methods.Add(2, 2);
+            Console.WriteLine(answer1);
+            int answer2 = Methods.Sub(10, 5);
+            Console.WriteLine(answer2);
+            int answer3 = Methods.Multiply(13, 28);
+            Console.WriteLine(answer3);
+            int answer4 = Methods.Divide(1026, 30);
+            Console.WriteLine(answer4);
         }
+
+        
     }
 }
