cbdecc11e3eeea4464643c5b1b7b43812dc30326
David Benjamin <davidbenjamin0685@gmail.com>
2023-01-31 13:11:07 -0700

    First exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -3,8 +3,19 @@
     public class Program
     {
         static void Main(string[] args)
+            
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite hockey team?");
+            string team = Console.ReadLine();
+            Console.WriteLine("Who is your best friend?");
+            string friend = Console.ReadLine();
+            Console.WriteLine("Where is your favorite place to visit?");
+            string place = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Cool name {name}, I see that the {team} is your favorite hockey team but I don't like hockey." +
+                $"\n I'll go to {place} while you hang out with your buddy {friend} in hockey land. Later alligator!");
         }
     }
 }
